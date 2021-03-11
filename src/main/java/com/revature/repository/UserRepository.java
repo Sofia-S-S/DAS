aeb6d333c1ee0d4d2e4ef27d8d95a4c5db9b5834
Michael Pearson <mpearson2516@yahoo.com>
2021-03-11 14:56:20 -0800

    Modified my registerNewPatient method

--- src/main/java/com/revature/repository/UserRepository.java
+++ src/main/java/com/revature/repository/UserRepository.java
@@ -1,11 +1,14 @@
 package com.revature.repository;
 
+import com.revature.model.Address;
+import com.revature.model.Login;
+import com.revature.model.Role;
 import com.revature.model.User;
 
 public interface UserRepository {
 
 	// New patients can register for an account
-	void registerNewPatient(User user);
+	void registerNewPatient(User user, Address address, Login login, Role role);
 	
 	// Users (doctors and patients) can update their infor
 	void updateInfo(User user);
